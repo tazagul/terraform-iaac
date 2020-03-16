d24ce6fc167d5e1924858f0abc7e42bf9dfc66a1
Cloud User <centos@ip-172-31-31-22.eu-west-2.compute.internal>
2020-03-16 04:39:45 +0000

    added environments

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-      bucket = "terraform-class-taza"
-      key = "path/to/my/vpc"
-      region = "us-east-1"
+backend "s3" {
+bucket = "terraform-class-taza"
+key = "tower/us-east-1/tools/virginia/tower.tfstate"
+region = "us-east-1"
   }
 }
